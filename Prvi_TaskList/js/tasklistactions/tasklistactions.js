c1db530aaeb89239bb515901a7c5e981a5f718c8
jinnxx <nadira.djogic@gmail.com>
2015-05-06 15:08:05 +0200

    Uredjenje koda
    dodani komentari u svim fajlovima

--- Prvi_TaskList/js/tasklistactions/tasklistactions.js
+++ Prvi_TaskList/js/tasklistactions/tasklistactions.js
@@ -2,28 +2,38 @@ var myapp = angular.module('MyApp', []);
 
 
 
-
+// main task list controller
 myapp.controller('TaskActionsCtrl', ['$scope', function($scope) {
+	// task item editor enabled
   $scope.editorEnabled = -1;
+  // input error flag
   $scope.inputerror = false;	
+  // input error message
   $scope.errormessage = "";
   $scope.errormessage2ManyTasks = "Limited to 20 tasks!";
   $scope.errormessageEmptyInput = "Enter your task!";
   
+  // task name input field model
   $scope.currenttasktext = "";
+  // every task has a different id
   $scope.taskidcounter = 1;
+  // max number of tasks
   $scope.maxtasks = 20;
+  // array for tasks and done tasks
   $scope.tasks = [];
   $scope.donetasks = [];
   
+  
+  // reset errors method
   $scope.resetErrors = function(){
 	  	$scope.inputerror = false;
 	  
   }
   
+  // add task method
   $scope.addTask = function(){
 	  $scope.resetErrors();
-	  
+		// error checking
 		if(!$scope.currenttasktext){
 			$scope.inputerror = true;
 			$scope.errormessage = $scope.errormessageEmptyInput;
@@ -39,7 +49,7 @@ myapp.controller('TaskActionsCtrl', ['$scope', function($scope) {
 		}
 		
 	  
-	  
+	  // new task
 	  var task = {
 		  taskid: $scope.taskidcounter,
 		  taskname: $scope.currenttasktext,
@@ -52,10 +62,11 @@ myapp.controller('TaskActionsCtrl', ['$scope', function($scope) {
 	  console.log("Added task: " + $scope.taskidcounter-1);
 	  console.log("Task list size: " + $scope.tasks.length);
 	  $scope.currenttasktext = "";
+	  // focus on the task name input field
 	  $( "#inputtask" ).focus();
 	  
   }
-  
+  // remove/delete active task
   $scope.removeTaskActive = function(index){
 		$scope.tasks.splice(index, 1);
 		$scope.resetErrors();
@@ -63,7 +74,7 @@ myapp.controller('TaskActionsCtrl', ['$scope', function($scope) {
 		console.log("Task list size: " + $scope.tasks.length);
   }
   
-    
+  // remove/delete done task  
   $scope.removeTaskDone = function(index){
 		$scope.donetasks.splice(index, 1);
 		$scope.resetErrors();
@@ -71,23 +82,21 @@ myapp.controller('TaskActionsCtrl', ['$scope', function($scope) {
 		console.log("Done Task list size: " + $scope.donetasks.length);
   }
   
+  // enable edit field for a chosen task
   $scope.enableEditTask = function(index){
 	  console.log("Enabling edit for: " + index + ", editablename:" + $scope.tasks[index].editabletaskname + " name: " +  $scope.tasks[index].taskname);
 	  $scope.tasks[index].editabletaskname = $scope.tasks[index].taskname;
 	  $scope.tasks[index].emptytaskinputerror = false;
 	  $scope.inputerror = false;
 	  $scope.editorEnabled = index; 
-	  
-	  
-	  
 	  console.log("Enabled edit for: " + index + ", editablename:" + $scope.tasks[index].editabletaskname + " name: " +  $scope.tasks[index].taskname);
 	  console.log("Focus: " + "#task"+ index);
   }
-  
+  // disable edit field for a chosen task
   $scope.disableEditTask = function(){
 	  $scope.editorEnabled = -1;  
   }
-  
+  // cancel edit for a chosen task
   $scope.cancelEditTask = function(index){
 	  console.log("Disabled edit for: " + index + ", editablename:" + $scope.tasks[index].editabletaskname + " name: " +  $scope.tasks[index].taskname);
 	  
@@ -95,7 +104,7 @@ myapp.controller('TaskActionsCtrl', ['$scope', function($scope) {
 	  console.log("Disabled edit for: " + index + ", editablename:" + $scope.tasks[index].editabletaskname);
   }
   
-  
+  // save edit for a chosen task
   $scope.saveEditTask = function(index){
 	  
 	if(!$scope.tasks[index].editabletaskname){
@@ -103,55 +112,52 @@ myapp.controller('TaskActionsCtrl', ['$scope', function($scope) {
 		$scope.tasks[index].emptytaskinputerror = true;
 		console.log("Task name not entered: " + $scope.tasks[index].emptytaskinputerror);
 		return;
-	}
-		  
+	}  
 	  console.log("Saving edit for: " + index + ", editablename:" + $scope.tasks[index].editabletaskname + " name: " +  $scope.tasks[index].taskname);
 	  $scope.tasks[index].taskname = $scope.tasks[index].editabletaskname;
-	  
 	  $scope.disableEditTask();
 	  
   }
   
-  
-  
-  
+  // mark task as done (move to done task list)
   $scope.marktaskdone = function(index){
 		var donetask = $scope.tasks.splice(index, 1);
-		
 		 $scope.donetasks.push(donetask[0]);
+		 $scope.resetErrors();
 		console.log("Done Task list size: " + $scope.donetasks.length);
 		
   }
-  
-    $scope.marktaskactive = function(index){
+  // mark task as active (move to active tasks list)
+  $scope.marktaskactive = function(index){
 		var activetask = $scope.donetasks.splice(index, 1);
-		
 		$scope.tasks.push(activetask[0]);
+		$scope.resetErrors();
 		console.log("Active Task list size: " + $scope.tasks.length);
 
   }
-  
+  // called on change for a task edit field
+  // removes error messages (main and for the task)
   $scope.keydownItem = function(index){
 		if($scope.tasks[index].editabletaskname){
-			$scope.inputerror = false;
+			$scope.resetErrors();
 			$scope.tasks[index].emptytaskinputerror = false;
 			return;
 		}
 	  
 	  
   }
-  
+  // called on change for the main task name input
+  // removes the main error message
     $scope.keydown = function(){
 		if($scope.currenttasktext){
 			$scope.resetErrors();
 			return;
-		}
-	  
-	  
+		}  
   }
  
 }]);
 
+// outputs the size of the window as it changes
 $(window).resize(function() {
   if ($(this).width() >= 1200) {
     console.log(">1200");
@@ -163,9 +169,7 @@ $(window).resize(function() {
     console.log("768<x<992");
   }
   else {
-	  
 	  console.log("<768");
-	  
   }
 });
 
